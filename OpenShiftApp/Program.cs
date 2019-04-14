47afcba5e23f2ca67d8ab20cfa115e38e421ea67
Ruslan <Ruslan@mail.ru>
2019-04-14 20:07:39 +0300

    eder

---
+++ OpenShiftApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace OpenShiftApp
+{
+    public class Program
+    {
+        public static IConfiguration Configuration { get; set; }
+        
+        public static void Main(string[] args)
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddEnvironmentVariables("GLOBAL_")
+                .Build();
+            
+            CreateWebHostBuilder().Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder() =>
+            WebHost.CreateDefaultBuilder()
+                .UseConfiguration(Configuration)
+                .UseStartup<Startup>();
+    }
+}
---
+++ OpenShiftApp/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace OpenShiftApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<AppSettings>(Program.Configuration.GetSection("App"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration configuration, IOptions<AppSettings> settings)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var json = JsonConvert.SerializeObject(settings.Value);
+            app.Run(async (context) => { await context.Response.WriteAsync(json); });
+        }
+    }
+
+    public class AppSettings
+    {
+        public string SimpleKey { get; set; }
+        public string[] ArrayKey { get; set; }
+        public AppSettingKey ObjectKey { get; set; }
+    }
+
+    public class AppSettingKey
+    {
+        public string SimpleKey2 { get; set; }
+        public string[] ArrayKey2 { get; set; }
+    }
+}
